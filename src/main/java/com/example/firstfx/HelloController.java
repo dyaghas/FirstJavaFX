ab0c79e2002978ce775d4e89c51b0ade3af63de2
Gustavo Cristian <diamonddyaghas@gmail.com>
2022-06-29 14:24:58 -0300

    possible login with a single username/password added

--- src/main/java/com/example/firstfx/HelloController.java
+++ src/main/java/com/example/firstfx/HelloController.java
@@ -2,16 +2,24 @@ package com.example.firstfx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    @FXML private Label welcomeText;
+    @FXML private TextField username;
+    @FXML private TextField password;
 
     private int num = 0;
 
     @FXML
     protected void submitBtnClick() {
-        num++;
-        welcomeText.setText("Welcome to JavaFX! " + num);
+
+        if(username.getText().equals("admin") && password.getText().equals("admin")) {
+            num++;
+            welcomeText.setText("Welcome to JavaFX! " + num);
+        } else {
+            num = 0;
+            welcomeText.setText("Incorrect credentials");
+        }
     }
 }
